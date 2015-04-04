1aa013d4d80004fd4df4397bad0735d7866b76d2
James Manlapid <jmanlapid@gmail.com>
2015-04-04 17:13:57 -0400

    Put everything back into a single route, will develop iron-router on branch

--- client/views/videos.js
+++
@@ -1,7 +0,0 @@
-Videos = new Mongo.Collection('videos');
-
-Template.videos.helpers({
-  videos: function () {
-    return Videos.find({});
-  }
-});
--- routes.js
+++
@@ -1,7 +0,0 @@
-Router.route('/upload', function () {
-	this.render('upload');
-});
-
-Router.route('/unapproved', function () {
-	this.render('unapproved');
-});
