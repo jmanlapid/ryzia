925caac31c95abd1f1467dc024ab337ca7baf004
James Manlapid <jmanlapid@gmail.com>
2015-04-04 19:37:10 -0400

    iron routed!

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
-Router.route('/upload', function () {
-	this.render('upload');
+Router.route('/', function () {
+	this.render('home');
 });
 
 Router.route('/unapproved', function () {
 	this.render('unapproved');
 });
+
+Router.route('/upload', function() {
+	this.render('upload');
+}); 
